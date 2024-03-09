0687a676d058e0ec8e76728e2072e3cfb9860466
kushalgandhi26 <kushalgandhi2601@gmail.com>
2024-03-08 20:21:27 -0500

    init commit

---
+++ pages/api/uploadFile.js
@@ -0,0 +1,34 @@
+import { createClient } from "@supabase/supabase-js";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const fileId = uuidv4();
+
+      const response = await axios.get(req.body.url, {
+        responseType: "arraybuffer", // Handle PDF as binary data
+        maxContentLength: 10240 * 10240, // Limit file size to 10MB
+      });
+
+      const uploadFile = await supabase.storage
+        .from("avatars")
+        .upload(`${fileId}.pdf`, response.data);
+
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .createSignedUrl(uploadFile.data.path, 3600);
+
+      res.status(200).json({ data, error });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
